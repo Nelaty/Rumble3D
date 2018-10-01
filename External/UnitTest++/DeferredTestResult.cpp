1dbb22b27d6c25a1187ad029cc7a2ad4e002e346
Matthias Gensheimer <matthias.e.gensheimer@gmail.com>
2018-10-01 15:26:53 +0200

    [fix] created custom project for the UnitTest++ library instead of using cmake, since cmake used absolute paths.

--- External/UnitTest++/DeferredTestResult.cpp
+++ External/UnitTest++/DeferredTestResult.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "Config.h"
 #ifndef UNITTEST_NO_DEFERRED_REPORTER
 
--- External/unittest-cpp-master/autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-test -e ./.autotools_aux || mkdir .autotools_aux
-
-libtoolize -cq
-aclocal -I m4 --install           # Generate aclocal
-autoconf                          # Generate configure script 
-autoheader                        # Generate config.h
-automake --add-missing --copy     # Generate Makefile.in and other scripts 
