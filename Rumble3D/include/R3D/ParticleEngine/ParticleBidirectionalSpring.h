07d2674e2c727c00cd456a844502bcc58fe96fe9
Matthias Gensheimer <matthias.e.gensheimer@gmail.com>
2019-03-17 21:58:14 +0100

    [add] section to user manual: IComputationInterface [add] ParticleBidirectionalSpring: acts like two mirrored ParticleSprings

---
+++ Rumble3D/include/R3D/ParticleEngine/ParticleBidirectionalSpring.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "R3D/ParticleEngine/IParticleForceGenerator.h"
+
+namespace r3
+{
+	/**
+	 * The ParticleBidirectionalSpring acts like two ParticleSprings, which 
+	 * connects two particles, so that both particles are affected.
+	 */
+	class ParticleBidirectionalSpring : public IParticleForceGenerator
+	{
+	public:
+		/**
+		* \brief ParticleSpring constructor.
+		* \param other The other particle, which will be pulled towards the
+		* particle in updateForce.
+		* \param springConstant The hardness of the spring.
+		* \param restLength The length at which no forces will be applied.
+		*/
+		ParticleBidirectionalSpring(Particle* other, 
+									real springConstant, 
+									real restLength);
+		~ParticleBidirectionalSpring();
+
+		/**
+		* \brief Calculates and changes the force in the force accumulator
+		* of a particle.
+		* \param particle The particle, on which the force should be applied to.
+		* \param duration The duration for which the force acts.
+		*/
+		void updateForce(Particle* particle, real duration) override;
+
+	private:
+		Particle* m_other;
+
+		real m_springConstant;
+		real m_restLength;
+	};
+}
