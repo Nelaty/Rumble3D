41d32e63b5d3410e8ed03e5fbc6a209136f996e5
Matthias Gensheimer <matthias.e.gensheimer@gmail.com>
2020-03-23 18:14:41 +0100

    [update] added implementation for a finite plane colliding with a sphere

--- Rumble3D/src/RigidBodyEngine/CollisionDetection/Algorithm/PlaneSphereCollisionAlgorithm.cpp
+++ Rumble3D/src/RigidBodyEngine/CollisionDetection/Algorithm/PlaneSphereCollisionAlgorithm.cpp
@@ -1,5 +1,7 @@
 #include "R3D/RigidBodyEngine/CollisionDetection/Algorithm/PlaneSphereCollisionAlgorithm.h"
 
+#include <glm/gtx/norm.inl>
+
 namespace r3
 {
 	PlaneSphereCollisionAlgorithm::PlaneSphereCollisionAlgorithm()
@@ -17,18 +19,20 @@ namespace r3
 		// Make sure we have contacts
 		if(collisionData.isFull()) return false;
 
+		// Cache the sphere position
+		const auto& position = rbSphere->getTransform().getPosition();
+
+		// Find the distance from the plane
+		real ballDistance =
+			glm::dot(plane->getNormal(), position) -
+			sphere->getRadius() - plane->getOffset();
+
+		// If sphere doesn't collide with plane, it will also not
+		// collide with a finite one
+		if(ballDistance >= 0) return false;
+
 		if(plane->isHalfSpace())
 		{
-			// Cache the sphere position
-			const auto& position = rbSphere->getTransform().getPosition();
-
-			// Find the distance from the plane
-			real ballDistance =
-				glm::dot(plane->getNormal(), position) -
-				sphere->getRadius() - plane->getOffset();
-
-			if(ballDistance >= 0) return false;
-
 			// Create the contact - it has a normal in the plane direction.
 			Contact* contact = collisionData.getAvailableContact();
 			contact->setContactNormal(plane->getNormal());
@@ -39,7 +43,37 @@ namespace r3
 			return true;
 		}
 
-		/// \todo: implement -> plane is not half space
-		return false;
+
+		const auto& planeTransform = rbPlane->getTransform();
+		const auto& planeDim = plane->getHalfSizes();
+		const auto radius = sphere->getRadius();
+
+		glm::vec3 relCenter = glm::transpose(planeTransform.getRotationMat()) *
+			(position - planeTransform.getPosition());
+
+		// Clamp each coordinate to the box.
+		glm::vec3 closestPt{};
+		closestPt.x = glm::clamp(relCenter.x, -planeDim.x, planeDim.x);
+		closestPt.y = ballDistance;
+		closestPt.z = glm::clamp(relCenter.z, -planeDim.y, planeDim.y);
+
+		// Check if there is a contact
+		const auto distance = glm::length2(closestPt - relCenter);
+		if(distance > radius * radius)
+		{
+			return false;
+		}
+
+		// Create the contact
+		auto closestPtWorld = rbPlane->getPointInWorldSpace(closestPt);
+		closestPtWorld.z *= -1; // hacky
+
+		auto* contact = collisionData.getAvailableContact();
+		glm::vec3 contactNormal = glm::normalize(closestPtWorld - position);
+		contact->setContactNormal(contactNormal);
+		contact->setContactPoint(closestPtWorld);
+		contact->setPenetration(sphere->getRadius() - sqrt(distance));
+		contact->setBodyData(rbPlane, rbSphere);
+		return true;
 	}
 }
