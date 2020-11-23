a00642dace449a6f5b2e1da9e3e3c827ec66bd93
Matthias Gensheimer <matthias.e.gensheimer@gmail.com>
2020-11-23 15:47:04 +0100

    [add] updated documentation

---
+++ docs/DoxygenDoc/html/search/classes_e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['velocityresolver_954',['VelocityResolver',['../classr3_1_1_velocity_resolver.html',1,'r3']]]
+];
---
+++ docs/DoxygenDoc/html/search/typedefs_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['sphcontext_5fptr_1756',['SPHContext_Ptr',['../classr3_1_1_i_s_p_h_integrator.html#af2ffad6574c84406daa97a6d0ad7b6ea',1,'r3::ISPHIntegrator::SPHContext_Ptr()'],['../classr3_1_1_i_s_p_h_kernel.html#a12a33f9f345b1146aabef6522bb4d405',1,'r3::ISPHKernel::SPHContext_Ptr()']]],
+  ['sphengine_5fptr_1757',['SPHEngine_Ptr',['../classr3_1_1_s_p_h_engine_computation_interface_base.html#acb27b2b48a2c7195aa1c14051371b622',1,'r3::SPHEngineComputationInterfaceBase']]],
+  ['system_5fptr_1758',['System_Ptr',['../classr3_1_1_s_p_h_engine.html#a0d63f5c8f2ccdbc7c7c51f9a54887d45',1,'r3::SPHEngine']]]
+];
